063ab737d362603b3c4961e58fc98da009749308
avinashpulluru <86039921+avinashpulluru@users.noreply.github.com>
2023-01-10 17:16:12 +0530

    Update variable.tf

--- LoadBalancer/i_LB/variable.tf
+++ LoadBalancer/i_LB/variable.tf
@@ -8,23 +8,26 @@ variable "LbRules" {
   type        = any
 }
 variable "prefix" {
+  type        = string
   description = "Prefix applied on the load balancer resources names."
 }
 
 variable "location" {
+  type        = string
   description = "Location of the load balancer, use the RG's location if not set"
-  default     = ""
 }
 
 variable "resource_group_name" {
+  type        = string
   description = "Resource group name of the load balancer."
 }
 
 variable "sku" {
+  type        = string
   description = "SKU of the load balancer."
 }
 
 variable "subnet_name" {
-  type    = any
+  type        = any
   description = "Subnet list"
 }
